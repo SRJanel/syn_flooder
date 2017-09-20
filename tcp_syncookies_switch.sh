3e2df39c86913a09f5f456fc86349a4ad0227b59
Unknown <nicolas.giannelos@epitech.eu>
2017-09-20 20:07:39 +0100

    tcp_syncookie kernel protection disable/enable script

--- tcp_syncookies_switch.sh
+++ tcp_syncookies_switch.sh
@@ -5,27 +5,29 @@
 ## Login SRJanel <n******.******s@epitech.eu>
 ## 
 ## Started on  Tue Sep 19 15:27:00 2017 
-## Last update Tue Sep 19 15:38:41 2017 
+## Last update Wed Sep 20 20:05:32 2017 
 ##
 
-## CODE REPETITION | change code
 TCP_SYNCOOKIES=`cat /proc/sys/net/ipv4/tcp_syncookies`
 RED="\033[1;31m"
 GREEN="\033[1;32m"
 NO_COLOR="\033[0m"
 
+check()
+{
+    if [ $1 -eq 0 ]; then
+	echo -e $2"TCP SynCookies "$3$NO_COLOR
+    else
+	echo "Run as root ?"
+    fi
+}
+
 if [ $TCP_SYNCOOKIES -eq 0 ]; then
-    echo 1 > /proc/sys/net/ipv4/tcp_syncookies &> /dev/null
-    if [ $? -eq 0 ]; then
-	echo -e $RED"TCP SynCookies enabled"$NO_COLOR
-    else
-	echo "Run as root ?"
-    fi
+    bash -c 'echo "1" > /proc/sys/net/ipv4/tcp_syncookies'
+    check $? $RED "enabled"
 else
-    echo 0 > /proc/sys/net/ipv4/tcp_syncookies &> /dev/null
-    if [ $? -eq 0 ]; then
-	echo -e $GREEN"TCP SynCookies disabled"$NO_COLOR
-    else
-	echo "Run as root ?"
-    fi
+    bash -c 'echo "0" > /proc/sys/net/ipv4/tcp_syncookies'
+    
+    check $? $GREEN "disabled"
+    
 fi
